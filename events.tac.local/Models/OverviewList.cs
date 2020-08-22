90565388f0c7e2d31173438a38773d0bc5413d18
Angel Anichin <anichin@gmail.com>
2020-08-22 12:32:52 -0600

    Lab 8.3 completed

---
+++ events.tac.local/Models/OverviewList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class OverviewList : List<OverviewItem>
+    {
+        public OverviewList()
+        {
+                
+        }
+    }
+}
