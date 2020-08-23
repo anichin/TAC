0d184c4576e8a4264f11b7947428a2314d91188d
Angel Anichin <anichin@gmail.com>
2020-08-22 18:46:58 -0600

    Lab 10.1 completed

---
+++ events.tac.local/Controllers/CommentsFormController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class CommentsFormController : Controller
+    {
+        // GET: SubscribeForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [TAC.Utils.Mvc.ValidateFormHandler]
+        public ActionResult Index(string comment, string name)
+        {
+            return View("Confirmation");
+        }
+    }
+}
