5cdaa35231f38acbee5ee405a520bc98bb093fe7
Angel Anichin <anichin@gmail.com>
2020-09-06 14:23:56 -0600

    Lab 12.4 completed

---
+++ events.tac.local/Business/EventsProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using events.tac.local.Models;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.Mvc.Presentation;
+
+namespace events.tac.local.Business
+{
+    public class EventsProvider
+    {
+        private const int PageSize = 4;
+        public EventsList GetEventsList(int pageNo)
+        {
+            var contextItem = RenderingContext.Current.ContextItem;
+            var indexname = $"events_{contextItem.Database.Name.ToLower()}_index";
+            var index = ContentSearchManager.GetIndex(indexname);
+            using (var context = index.CreateSearchContext())
+            {
+                var results = context.GetQueryable<EventDetails>()
+                .Where(i => i.Paths.Contains(contextItem.ID))
+                .Where(i => i.Language == contextItem.Language.Name)
+                .Page(pageNo, PageSize)
+                .GetResults();
+                return new EventsList()
+                {
+                    Events = results.Hits.Select(h => h.Document).ToArray(),
+                    PageSize = PageSize,
+                    TotalResultCount = results.TotalSearchResults
+                };
+            }
+        }
+    }
+}
---
+++ events.tac.local/Controllers/EventsListController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using events.tac.local.Business;
+
+namespace events.tac.local.Controllers
+{
+    public class EventsListController : Controller
+    {
+        private readonly EventsProvider _provider;
+        public EventsListController() : this(new EventsProvider()) { }
+        public EventsListController(EventsProvider provider)
+        {
+            _provider = provider;
+        }
+
+        // GET: EventsList
+        public ActionResult Index(int page = 1)
+        {
+            var eventsList = _provider.GetEventsList(page - 1);
+            return View(eventsList);
+        }
+    }
+}
