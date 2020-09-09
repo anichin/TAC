a6b8caf6acaa0b2241429269d0b688926878dfe6
Angel Anichin <anichin@gmail.com>
2020-09-08 19:46:10 -0600

    Lab 15.1 completed

---
+++ events.tac.local/Business/ServicesConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using events.tac.local.Business.Navigation;
+using events.tac.local.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Mvc.Presentation;
+
+namespace events.tac.local.Business
+{
+    public class ServicesConfigurator : Sitecore.DependencyInjection.IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<NavigationModelBuilder>();
+            serviceCollection.AddTransient<NavigationController>();
+            serviceCollection.AddTransient<RenderingContext>((r) => RenderingContext.Current);
+        }
+    }
+}
---
+++ events.tac.local/Controllers/NavigationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using events.tac.local.Business.Navigation;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace events.tac.local.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private readonly NavigationModelBuilder _modelBuilder;
+        private readonly RenderingContext _renderingContext;
+        public NavigationController(NavigationModelBuilder modelBuilder, RenderingContext renderingContext)
+        {
+            _modelBuilder = modelBuilder;
+            _renderingContext = renderingContext;
+        }
+
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            Item currentItem = _renderingContext.ContextItem;
+            Item section = currentItem.Axes.GetAncestors()
+                .FirstOrDefault(i => i.TemplateName == "Event Section");
+            var model = _modelBuilder.CreateNavigationMenu(section, currentItem);
+
+            return View();
+        }
+    }
+}
